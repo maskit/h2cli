f160497d455639bf9e58046ea6afd2db7d4829d1
Masakazu Kitajo <m4sk17@gmail.com>
2014-06-29 01:14:25 +0900

    add tests for PING frame

---
+++ test/test-frame.js
@@ -0,0 +1,53 @@
+var assert = require("assert")
+var fs = require('fs');
+var h2frame = require('../lib/frame');
+
+describe('Http2PingFrame:', function () {
+    describe('Created frame', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should have 8 octets of payload', function () {
+            assert.equal(frame.length, 8);
+        });
+    });
+    describe('Created frame', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should have type value of PING frame', function () {
+            assert.equal(frame.flags, 0);
+        });
+    });
+    describe('Created frame', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should have no flags', function () {
+            assert.equal(frame.flags, 0);
+        });
+    });
+    describe('Created frame', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should have streamId zero', function () {
+            assert.equal(frame.streamId, 0);
+        });
+    });
+    describe('#getBuffer()', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should return a buffer, and its length should be 16 octets', function () {
+            assert(frame.getBuffer() instanceof Buffer);
+            assert.equal(frame.getBuffer().length, 16);
+        });
+    });
+    describe('#getPayload()', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should return a buffer, and its length should be 8 octets', function () {
+            assert(frame.getPayload() instanceof Buffer);
+            assert.equal(frame.getPayload().length, 8);
+        });
+    });
+    describe('#setPayload()', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should set the given data into the buffer', function () {
+            var expected = new Buffer([0x11, 0x22, 0x33, 0x44, 0xAA, 0xBB, 0xCC, 0xDD]);
+            frame.setPayload(expected);
+            assert.deepEqual(frame.getPayload(), expected);
+            assert.deepEqual(frame.getBuffer().slice(8, 16), expected);
+        });
+    });
+});
