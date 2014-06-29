b157aa1644fc0a88eec5e92950b2f1133f4aa7b6
Masakazu Kitajo <m4sk17@gmail.com>
2014-06-29 17:04:35 +0900

    add tests for Http2Frame

--- test/test-frame.js
+++ test/test-frame.js
@@ -2,6 +2,90 @@ var assert = require("assert")
 var fs = require('fs');
 var h2frame = require('../lib/frame');
 
+describe('Http2Frame:', function () {
+    describe('HEADER_SIZE', function () {
+        it('should be a constant value for header size', function () {
+            assert.equal(h2frame.Http2Frame.HEADER_SIZE, 8);
+            h2frame.Http2PingFrame.FLAG_ACK = 9999;
+            assert.equal(h2frame.Http2Frame.HEADER_SIZE, 8);
+        });
+    });
+    describe('length property', function () {
+        it('should be the length of payload', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 0);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 1);
+            frame = new h2frame.Http2Frame(new Buffer([0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 256);
+            frame = new h2frame.Http2Frame(new Buffer([0x3F, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 16383);
+            frame = new h2frame.Http2Frame(new Buffer([0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 0);
+        });
+        it('should be writable', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.length, 0);
+            frame.length = 9999;
+            assert.equal(frame.length, 9999);
+        });
+    });
+    describe('type property', function () {
+        it('should be the type of frame', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.type, 1);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.type, 255);
+        });
+        it('should be writable', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.type, 0);
+            frame.type = 99;
+            assert.equal(frame.type, 99);
+        });
+    });
+    describe('flags property', function () {
+        it('should be the flags which the frame has', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.flags, 0x01);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.flags, 0xFF);
+        });
+        it('should be writable', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.flags, 0);
+            frame.flags |= 0xFF;
+            assert.equal(frame.flags, 0xFF);
+        });
+    });
+    describe('streamId property', function () {
+        it('should be the streamId of the frame', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.streamId, 0);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]));
+            assert.equal(frame.streamId, 1);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x7F, 0xFF, 0xFF, 0xFF]));
+            assert.equal(frame.streamId, Math.pow(2, 31) -1);
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF]));
+            assert.equal(frame.streamId, Math.pow(2, 31) -1);
+        });
+        it('should be writable', function () {
+            var frame;
+            frame = new h2frame.Http2Frame(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]));
+            assert.equal(frame.streamId, 0);
+            frame.streamId = 99999;
+            assert.equal(frame.streamId, 99999);
+        });
+    });
+});
+
 describe('Http2PingFrame:', function () {
     describe('A frame created without buffer', function () {
         var frame = new h2frame.Http2PingFrame();
