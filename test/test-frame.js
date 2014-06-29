0e429c176c07319bf547460afd65ed9d56a4f5d9
Masakazu Kitajo <m4sk17@gmail.com>
2014-06-29 17:55:35 +0900

    add tests for Http2FrameFactory

--- test/test-frame.js
+++ test/test-frame.js
@@ -155,3 +155,92 @@ describe('Http2PingFrame:', function () {
         });
     });
 });
+
+describe('Http2FrameFactory:', function () {
+    describe('#createFrame()', function () {
+        it('should create an appropriate frame instance', function () {
+            var frame, buf;
+            // DATA
+            buf = new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2DataFrame);
+            // HEADERS
+            buf = new Buffer([0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2HeadersFrame);
+            // PRIORITY
+            buf = new Buffer([0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h1frame.Http2PriorityFrame);
+            // RST_STREAM
+            buf = new Buffer([0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2RstStreamFrame);
+            // SETTINGS
+            buf = new Buffer([0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2SettingsFrame);
+            // PUSH_PROMISE
+            buf = new Buffer([0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2PushPromiseFrame);
+            // PING
+            buf = new Buffer([0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2PingFrame);
+            // GOAWAY
+            buf = new Buffer([0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2GoawayFrame);
+            // WINDOW_UPDATE
+            buf = new Buffer([0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2WindowUpdateFrame);
+            // CONTINUATION
+            buf = new Buffer([0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2ContinuationFrame);
+        });
+        it('should create an Http2Frame instance if type code is unknown', function () {
+            var buf, frame;
+            buf = new Buffer([0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2Frame);
+        });
+    });
+    describe('#createRequestFrames()', function () {
+        it('should create one HEADERS frame and zero or more CONTINUATION frames', function () {
+            var frames, blockSize;
+            var hpackMock = {
+                'encode': function (headers) {
+                    return new Buffer(blockSize);
+                }
+            };
+            blockSize = 0;
+            frames = h2frame.Http2FrameFactory.createRequestFrames({}, hpackMock);
+            assert.equal(frames.length, 1);
+            blockSize = 1;
+            frames = h2frame.Http2FrameFactory.createRequestFrames({}, hpackMock);
+            assert.equal(frames.length, 1);
+            blockSize = 16383;
+            frames = h2frame.Http2FrameFactory.createRequestFrames({}, hpackMock);
+            assert.equal(frames.length, 1);
+            blockSize = 16383;
+            frames = h2frame.Http2FrameFactory.createRequestFrames({}, hpackMock);
+            assert.equal(frames.length, 2);
+        });
+    });
+    describe('#registerFrame()', function () {
+        it('should add a supported frame type', function () {
+            var buf, frame, newFrameClass;
+            buf = new Buffer([0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof h2frame.Http2Frame);
+            newFrameClass = function () {};
+            h2frame.Http2FrameFactory.registerFrame(0xFF, newFrameClass);
+            buf = new Buffer([0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00]);
+            frame = h2frame.Http2FrameFactory.createFrame(buf);
+            assert(frame instanceof newFrameClass);
+        });
+    });
+});
