8a300075bfc96faf9bee97fde72471d24eaf32b5
Masakazu Kitajo <m4sk17@gmail.com>
2014-06-29 16:31:22 +0900

    add tests for PING frame

--- test/test-frame.js
+++ test/test-frame.js
@@ -3,28 +3,48 @@ var fs = require('fs');
 var h2frame = require('../lib/frame');
 
 describe('Http2PingFrame:', function () {
-    describe('Created frame', function () {
+    describe('A frame created without buffer', function () {
         var frame = new h2frame.Http2PingFrame();
         it('should have 8 octets of payload', function () {
             assert.equal(frame.length, 8);
         });
     });
-    describe('Created frame', function () {
+    describe('A frame created without buffer', function () {
         var frame = new h2frame.Http2PingFrame();
         it('should have type value of PING frame', function () {
             assert.equal(frame.flags, 0);
         });
     });
-    describe('Created frame', function () {
+    describe('A frame created without buffer', function () {
         var frame = new h2frame.Http2PingFrame();
         it('should have no flags', function () {
             assert.equal(frame.flags, 0);
         });
     });
-    describe('Created frame', function () {
+    describe('A frame created without buffer', function () {
         var frame = new h2frame.Http2PingFrame();
         it('should have streamId zero', function () {
             assert.equal(frame.streamId, 0);
+        });
+    });
+    describe('A frame created with buffer', function () {
+        var buf = new Buffer([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10]);
+        var frame = new h2frame.Http2PingFrame(buf);
+        it('should have the buffer', function () {
+            assert.equal(frame.getBuffer(), buf);
+        });
+    });
+    describe('FLAG_ACK', function () {
+        it('should be a constant value for ACK flag', function () {
+            assert.equal(h2frame.Http2PingFrame.FLAG_ACK, 0x1);
+            h2frame.Http2PingFrame.FLAG_ACK = 9999;
+            assert.equal(h2frame.Http2PingFrame.FLAG_ACK, 0x1);
+        });
+    });
+    describe('#toString()', function () {
+        var frame = new h2frame.Http2PingFrame();
+        it('should return a string containing frame name', function () {
+            assert.notEqual(frame.toString().indexOf('PING'), -1);
         });
     });
     describe('#getBuffer()', function () {
