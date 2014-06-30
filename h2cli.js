c2cc4f346ae4c2d6e750bce5517ca0431a1aef5c
Masakazu Kitajo <m4sk17@gmail.com>
2014-07-01 07:27:27 +0900

    support POST method

--- h2cli.js
+++ h2cli.js
@@ -171,6 +171,34 @@ commands.head = function (args, callback) {
 };
 
 commands.post = function (args, callback) {
+    if (!args[1]) {
+        console.log('usage: post URL data');
+        callback();
+        return;
+    }
+    var response = {};
+    h2client.request(args[1], 'POST', args[2], {
+        'header': function (headers) {
+            response.header = headers;
+        },
+        'data': function (data) {
+            if (response.body) {
+                response.body += data;
+            } else {
+                response.body = data;
+            }
+        },
+        'close': function () {
+            console.log('Response');
+            console.log(' Header');
+            response.header.forEach(function (h) {
+                console.log('  ' + h[0] + ': ' + h[1]);
+            });
+            console.log(' Body');
+            console.log('  ' + response.body.toString());
+            callback();
+        },
+    });
 };
 
 commands.close = function (args, callback) {
