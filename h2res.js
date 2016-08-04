1de063222ddb6dc037a176807261e69e31ba46e6
Masakazu Kitajo <m4sk17@gmail.com>
2016-08-05 00:28:04 +0900

    fix a bug in h2res

--- h2res.js
+++ h2res.js
@@ -6,19 +6,21 @@ var fs = require('fs');
 
 var tmp = new Buffer(0);
 process.stdin.on('data', function (chunk) {
-    while (chunk.length >= 9) {
+    chunk = Buffer.concat([tmp, chunk]);
+    tmp = new Buffer(0);
+    while (chunk !== null && chunk.length >= 9) {
         f = h2cli.frame.Http2FrameFactory.createFrame(chunk);
         console.log(h2cli.util.printFrame(f, true));
         if (f instanceof h2cli.frame.Http2DataFrame && f.length > 0) {
             console.log(" Data:");
             console.log(f.getData().toString());
         }
-        if (chunk.length >= f.length) {
+        if (chunk.length >= f.length + 9) {
             chunk = chunk.slice(f.length + 9);
+        } else {
+            tmp = chunk;
+            chunk = null;
         }
-    }
-    if (chunk.length !== 0) {
-        tmp = Buffer.concat([tmp, chunk]);
     }
 });
 
